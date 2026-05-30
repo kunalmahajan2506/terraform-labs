7d5e890eb87cecbace94ca9d497bedcca4b5cf14
Girishmhj-45 <kunalmahajan2546@gmail.com>
2026-05-30 18:16:53 +0530

    Update main.tf

--- ec2-modules/main.tf
+++ ec2-modules/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "web_server" {
   source = "../modules/ec2"
 
-  ami_id        = "ami-xxxxxxxx"
-  instance_type = "t2.micro"
-  name          = "my-"
+  ami_id        = "var.ami_id"
+  instance_type = "var.instance_type"
+  name          = "var.instance_name"
 }
